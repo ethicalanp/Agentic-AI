909f0aae91774d11bc72704e83d3917b635d063d
ethicalanp <ethicalanp66@gmail.com>
2025-03-28 18:14:48 +0530

    added multiagent

---
+++ BasicAgent/multiagent.py
@@ -0,0 +1,8 @@
+from agno.agent import Agent 
+from agno.models.openai import OpenAIChat 
+from agno.models.groq import Groq 
+from agno.tools.duckduckgo import DuckDuckGoTools 
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
